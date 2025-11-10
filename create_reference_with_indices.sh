cf173af20eb87497c4db5a45843a56152ae821f0
Anil Thanki <thanki.anil@gmail.com>
2025-11-10 13:38:52 +0000

    makes it paramerised per species

--- create_reference_with_indices.sh
+++ create_reference_with_indices.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=nf-core-references
-#SBATCH --time=01-23:00:00
+#SBATCH --time=07-00:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=12000
 #SBATCH --output=nf-core-references-%j-%x.out
@@ -8,11 +8,16 @@
 #SBATCH --partition=production
 
 source activate /hps/software/users/ma/service/isl/conda/config/envs/nextflow=25.10.0
-
+timestamp=$(date +"%Y_%m_%d_%I_%M_%p")
 NXF_ANSI_LOG=false
 export NXF_TTY_WIDTH=999
+
+
+i=$1
+
 nextflow run nf-core-references/main.nf \
-	--input datasheet.yaml \
-	--outdir $BULK_REFERENCE_DIR/test \
+	--input ${i} \
+	--outdir $BULK_REFERENCES_DIR \
 	--tools "star,salmon,kallisto,faidx,createsequencedictionary,intervals,sizes,tabix" \
-	-with-trace trace.txt
+	-with-trace $BULK_REFERENCES_DIR/nf-core-references_trace_$i_$timestamp.txt 
+
